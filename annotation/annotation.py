608818eedc6fd5f5ba9de2c6d7c7979702b42ea8
Muyang Xu <74348157+Shirley-Cullen@users.noreply.github.com>
2025-03-13 00:20:13 -0400

    Update annotation.py

--- annotation/annotation.py
+++ annotation/annotation.py
@@ -41,7 +41,7 @@ def main():
     pipe = pipeline('text-generation', model=model, tokenizer=tokenizer, torch_dtype=torch.float16)
 
     intro_info = """You are a smart assistant designed to help come up with reading comprehension questions. You will be given a web-crawled document relevant to topics about Pittsburgh and Carnegie Mellon University (CMU) such as general information/history, events, music, sports, and culture."""
-    task = """Based on the document, generate exactly 10 question and answer pairs covering different content topics."""
+    task = """Based on the document, generate 5~10 question and answer pairs covering different content topics."""
     requirement = """Each question must be independently answerable and the answers must be directly and exactly found in the document."""
     answer_format = """For each pair, output in this exact format without any extra text:
         Q: "YOUR_QUESTION_HERE", Ans: "YOUR_ANSWER_HERE"."""
@@ -88,7 +88,12 @@ def main():
         except:
             print("Failed to retrieve questions", SOURCE)
             continue
+
+        # TODO: Figure out why some annotations cannot retrieve questions??? len(questions) == 0????
         print("Questions generated successfully:", SOURCE, len(questions))
+        if len(questions) == 0:
+            with open("failed_annotations.txt", "a") as file:
+                file.write(f"{SOURCE}:\n" + result[0]['generated_text'][1]["content"] + "\n\n")
         for q, a in questions:
             new_row = pd.DataFrame({"Questions": [q], "Answers": [a]})
             topics_df[TOPIC] = pd.concat([topics_df[TOPIC], new_row], ignore_index=True)
