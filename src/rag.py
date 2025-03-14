c954dd7723d3078bc34d58f7498230d390ac1875
LIANG-SN <liangsn2001@outlook.com>
2025-03-14 00:16:06 +0000

    update rag for splitting text

--- src/rag.py
+++ src/rag.py
@@ -2,13 +2,16 @@
 import argparse
 import os
 import torch
+import gc
+import pandas as pd
 from langchain_core.documents import Document
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_chroma import Chroma
 from langchain_huggingface import HuggingFacePipeline
 from langchain.chains import RetrievalQA
 from langchain.chains import create_retrieval_chain
-from transformers import pipeline
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from transformers import pipeline, AutoTokenizer, AutoModelForSeq2SeqLM
 from langchain_community.document_loaders import DirectoryLoader
 
 
@@ -22,13 +25,19 @@ def arg_parser():
                         default="../data/rag_dummy_data/questions.txt")
     parser.add_argument('--answers_file', type=str, 
                         default="../data/rag_dummy_data/answers.txt")
-    parser.add_argument('--batch_size', type=int, default=8,
+    parser.add_argument('--batch_size', type=int, default=1,
                         help="Batch size for inference")
     parser.add_argument("--generation_model_name", type=str,
                         default="google/flan-t5-base")
     parser.add_argument("--embedder_model_name", type=str,
                         default="sentence-transformers/all-mpnet-base-v2")
-    
+    parser.add_argument("--annotation_data_format", type=str,
+                        default="csv")
+    parser.add_argument("--annotation_csv", type=str,
+                        default="../data/annotation_data/annotation_qa/Culture.csv")
+    parser.add_argument("--output_path", type=str,
+                        default="../data/output/output.csv")
+    parser.add_argument("--topk", type=int, default=3)
     return parser.parse_args()
 
 def calculate_metrics(predictions, ground_truths):
@@ -93,7 +102,13 @@ def batch_inference(qa_chain, questions, batch_size, ground_truths=None):
         batch = questions[i:i+batch_size]
         batch_inputs = [{"query": q} for q in batch]
         # Use invoke to process the batch
-        batch_outputs = qa_chain.batch(batch_inputs)
+        with torch.no_grad():
+            if batch_size > 1:
+                batch_outputs = qa_chain.batch(batch_inputs)
+            else:
+                batch_outputs = [qa_chain.invoke(batch_inputs[0])]
+        torch.cuda.empty_cache()
+        gc.collect()
         
         batch_predictions = []
         for output in batch_outputs:
@@ -130,36 +145,58 @@ def main():
     # Load retrieval documents from the specified directory
     loader = DirectoryLoader(args.retrieval_dir, glob="**/*.txt")
     docs = loader.load()
+    splitter = RecursiveCharacterTextSplitter(chunk_size=400, chunk_overlap=0)
+    splitted_docs = splitter.split_documents(docs)
+    print(f"Retrieval documents loaded.")
 
     # Initialize embeddings using a pre-trained model
     embeddings = HuggingFaceEmbeddings(model_name=args.embedder_model_name, model_kwargs={"device": device})
 
     # Build a Chroma vector store from the documents and embeddings.
-    vectorstore = Chroma.from_documents(docs, embeddings, persist_directory="./chroma_db")
-    retriever = vectorstore.as_retriever()
+    vectorstore = Chroma.from_documents(splitted_docs, embeddings, persist_directory="./chroma_db")
+    retriever = vectorstore.as_retriever(search_kwargs={"k": args.topk})
+    print(f"Vector Store finish.")
 
     # Initialize a Hugging Face pipeline for text generation using an open-source model.
-    pipe = pipeline("text2text-generation", model=args.generation_model_name, max_length=256, device=device)
+    tokenizer = AutoTokenizer.from_pretrained(args.generation_model_name)
+    model = AutoModelForSeq2SeqLM.from_pretrained(args.generation_model_name, device_map="auto")
+    model.config.use_cache = False
+    model.eval()
+    pipe = pipeline("text2text-generation", model=model, tokenizer=tokenizer, max_length=256)
     llm = HuggingFacePipeline(pipeline=pipe)
 
     # Create the RAG system using RetrievalQA
     qa_chain = RetrievalQA.from_chain_type(llm=llm, chain_type="stuff", retriever=retriever)
 
-    # Load questions from file
-    with open(args.questions_file, "r", encoding="utf-8") as f:
-        questions = [line.strip() for line in f if line.strip()]
-    print(f"Loaded {len(questions)} questions.")
+    if args.annotation_data_format == "csv":
+        # load questions and answers
+        df = pd.read_csv(args.annotation_csv)
+        questions = df['Questions'].tolist()
+        ground_truths = df['Answers'].tolist()
 
-    # Load ground truth answers if provided
-    ground_truths = []
-    if args.answers_file and os.path.exists(args.answers_file):
-        with open(args.answers_file, "r", encoding="utf-8") as f:
-            ground_truths = [line.strip() for line in f if line.strip()]
-        print(f"Loaded {len(ground_truths)} answers.")
+    elif args.annotation_data_format == "txt":
+        # Load questions from file
+        with open(args.questions_file, "r", encoding="utf-8") as f:
+            questions = [line.strip() for line in f if line.strip()]
+        print(f"Loaded {len(questions)} questions.")
+
+        # Load ground truth answers if provided
+        ground_truths = []
+        if args.answers_file and os.path.exists(args.answers_file):
+            with open(args.answers_file, "r", encoding="utf-8") as f:
+                ground_truths = [line.strip() for line in f if line.strip()]
+            print(f"Loaded {len(ground_truths)} answers.")
+    
 
     # Perform batch inference with customizable batch size
     predictions = batch_inference(qa_chain, questions, args.batch_size, ground_truths if ground_truths else None)
     print("Batch inference complete.\n")
+
+    # Add the predictions as a new column in the DataFrame
+    df['Predictions'] = predictions
+
+    # Save the updated DataFrame to a new CSV file
+    df.to_csv(args.output_path, index=False)
 
     # Print overall predictions
     for question, answer in zip(questions, predictions):
